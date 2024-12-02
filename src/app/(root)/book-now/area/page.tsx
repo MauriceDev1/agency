579f9eb0a1c844a0d8932bbad1e4695ce8bd76e0
DirkHeydenrych <dirk.heydenrycj@gmail.com>
2024-12-02 20:59:48 +0200

    Area

--- src/app/(root)/book-now/area/page.tsx
+++ src/app/(root)/book-now/area/page.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 
 const Area = () => {
-  return (
-    <ul className="list-disc pl-5 space-y-2">
-      <li className="text-gray-700">Option 1</li>
-      <li className="text-gray-700">Option 2</li>
-      <li className="text-gray-700">Option 3</li>
-    </ul>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState<number | null>(null);
 
-export default Area
+  const handleOptionClick = (optionIndex: number) => {
+    setSelectedOption(selectedOption === optionIndex ? null : optionIndex);
+  };
+
+  return (
+    <div>
+      <ul className="list-disc pl-5 space-y-2">
+        <li className="text-gray-700 cursor-pointer" onClick={() => handleOptionClick(1)}>
+          Option 1
+          {selectedOption === 1 && (
+            <ul className="pl-5 mt-2 space-y-1">
+              <li className="text-gray-600">Sub-option 1.1</li>
+              <li className="text-gray-600">Sub-option 1.2</li>
+              <li className="text-gray-600">Sub-option 1.3</li>
+            </ul>
+          )}
+        </li>
+        <li className="text-gray-700 cursor-pointer" onClick={() => handleOptionClick(2)}>
+          Option 2
+          {selectedOption === 2 && (
+            <ul className="pl-5 mt-2 space-y-1">
+              <li className="text-gray-600">Sub-option 2.1</li>
+              <li className="text-gray-600">Sub-option 2.2</li>
+              <li className="text-gray-600">Sub-option 2.3</li>
+            </ul>
+          )}
+        </li>
+        <li className="text-gray-700 cursor-pointer" onClick={() => handleOptionClick(3)}>
+          Option 3
+          {selectedOption === 3 && (
+            <ul className="pl-5 mt-2 space-y-1">
+              <li className="text-gray-600">Sub-option 3.1</li>
+              <li className="text-gray-600">Sub-option 3.2</li>
+              <li className="text-gray-600">Sub-option 3.3</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Area;
