6c3ac963243897dd657e52226e37c76b1193829e
DirkHeydenrych <dirk.heydenrycj@gmail.com>
2024-12-02 20:19:44 +0200

    Correct Format

--- src/app/(root)/book-now/layout.tsx
+++ src/app/(root)/book-now/layout.tsx
@@ -1,37 +1,11 @@
 import Link from "next/link";
+import Links from './booknow'
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    const Links = [
-        {
-            id: 1,
-            name: "Area",
-            link: "/book-now/area",
-        },
-        {
-            id: 2,
-            name:"City",
-            link:"/book-now/city"
-        },
-        {
-            id: 3,
-            name:"Duration",
-            link:"/book-now/duration"
-        },
-        {
-            id: 4,
-            name:"Gender",
-            link:"/book-now/gender"
-        },
-        {
-            id: 5,
-            name:"Time",
-            link:"/book-now/time"
-        },
-    ]
   return (
     <div className="bg-red-500 h-56">
         <div className="w-full flex justify-between">
